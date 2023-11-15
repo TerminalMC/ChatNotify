e4929ed4113c8cfc9149b8d327c48b9a7cd235b4
NotRyken <notryken@gmail.com>
2023-11-15 15:09:57 +0800

    Add ModMenu support and fix icon and README

---
+++ fabric/src/main/java/notryken/chatnotify/gui/screen/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package notryken.chatnotify.gui.screen;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi
+{
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory()
+    {
+        return ScreenLauncher.MainOptionsScreen::new;
+    }
+}
