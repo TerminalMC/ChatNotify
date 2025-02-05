20f81746a33e4257682f1ce91a7bed8cc0c99ad0
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-02-05 10:28:19 +0800

    Update to mc1.21.3

--- common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/NotifOptionList.java
+++ common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/NotifOptionList.java
@@ -27,7 +27,7 @@ import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.*;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.FastColor;
+import net.minecraft.util.ARGB;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
@@ -555,8 +555,8 @@ public class NotifOptionList extends DragReorderList {
                         // Update color of main button and field
                         mainButton.setMessage(mainButton.getMessage().copy().withColor(color));
                         float[] hsv = new float[3];
-                        Color.RGBtoHSB(FastColor.ARGB32.red(color), FastColor.ARGB32.green(color),
-                                FastColor.ARGB32.blue(color), hsv);
+                        Color.RGBtoHSB(ARGB.red(color), ARGB.green(color),
+                                ARGB.blue(color), hsv);
                         if (hsv[2] < 0.1) colorField.setTextColor(0xFFFFFF);
                         else colorField.setTextColor(color);
                     }
