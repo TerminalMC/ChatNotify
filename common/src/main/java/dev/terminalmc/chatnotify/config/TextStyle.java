b01e409456b70a85dfb56b17b255f7f29a3eb765
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-02-10 20:21:22 +0800

    Update to mc1.21.4

--- common/src/main/java/dev/terminalmc/chatnotify/config/TextStyle.java
+++ common/src/main/java/dev/terminalmc/chatnotify/config/TextStyle.java
@@ -112,7 +112,8 @@ public class TextStyle {
 
     public Style getStyle() {
         return new Style(
-                doColor ? TextColor.fromRgb(color) : null,
+                doColor ? TextColor.fromRgb(color) : null, 
+                null,
                 bold != FormatMode.DISABLED ? bold == FormatMode.ON : null,
                 italic != FormatMode.DISABLED ? italic == FormatMode.ON : null,
                 underlined != FormatMode.DISABLED ? underlined == FormatMode.ON : null,
