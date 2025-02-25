7137a1d04bae50b88e1654f4a388f0e3408c8254
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-02-25 21:03:09 +0800

    Update to mc1.21.4

--- common/src/main/java/dev/terminalmc/chatnotify/gui/widget/field/FakeTextField.java
+++ common/src/main/java/dev/terminalmc/chatnotify/gui/widget/field/FakeTextField.java
@@ -32,18 +32,18 @@ public class FakeTextField extends TextField {
     }
 
     @Override
-    public boolean clicked(double mouseX, double mouseY) {
+    public boolean isMouseOver(double mouseX, double mouseY) {
         // Skip the 'active' requirement
         return (visible
                 && mouseX >= (double)getX()
                 && mouseY >= (double)getY()
-                && mouseX < (double)(getX() + getWidth())
-                && mouseY < (double)(getY() + getHeight()));
+                && mouseX < (double)getRight()
+                && mouseY < (double)getBottom());
     }
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        if (clicked(mouseX, mouseY)) {
+        if (isMouseOver(mouseX, mouseY)) {
             onClick(mouseX, mouseY);
             return true;
         }
