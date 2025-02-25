6ebc1b1ea580f06bd014ff4d015cc508967e7305
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-02-25 20:44:05 +0800

    Update to mc1.21.3

--- common/src/main/java/dev/terminalmc/chatnotify/gui/toast/NotificationToast.java
+++ common/src/main/java/dev/terminalmc/chatnotify/gui/toast/NotificationToast.java
@@ -20,7 +20,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.components.toasts.ToastManager;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FormattedCharSequence;
@@ -40,6 +41,7 @@ public class NotificationToast implements Toast {
 
     private final int lineHeight;
     private final List<FormattedCharSequence> messageLines;
+    private Toast.Visibility wantedVisibility;
 
     public NotificationToast(Component message) {
         this.messageLines = Minecraft.getInstance().font.split(message, WIDTH - X_MARGIN * 2);
@@ -47,12 +49,22 @@ public class NotificationToast implements Toast {
     }
 
     @Override
-    public @NotNull Visibility render(@NotNull GuiGraphics graphics,
-                                      @NotNull ToastComponent component, long elapsedTime) {
-        Font font = component.getMinecraft().font;
+    public @NotNull Visibility getWantedVisibility() {
+        return wantedVisibility;
+    }
+
+    @Override
+    public void update(@NotNull ToastManager manager, long elapsedTime) {
+        this.wantedVisibility =
+                elapsedTime < DISPLAY_TIME * manager.getNotificationDisplayTimeMultiplier()
+                        ? Visibility.SHOW : Visibility.HIDE;
+    }
+
+    @Override
+    public void render(@NotNull GuiGraphics graphics, @NotNull Font font, long elapsedTime) {
         if (messageLines.size() <= 1) {
             // Message fits in a single line, render a single sprite
-            graphics.blitSprite(BACKGROUND_SPRITE, 0, 0, WIDTH, height());
+            graphics.blitSprite(RenderType::guiTextured, BACKGROUND_SPRITE, 0, 0, WIDTH, height());
         } else {
             // Message requires multiple lines, stretch vertically by rendering
             // multiple sprites
@@ -88,9 +100,6 @@ public class NotificationToast implements Toast {
                         X_MARGIN, Y_MARGIN + lineHeight * j, -1, false);
             }
         }
-
-        return elapsedTime < DISPLAY_TIME * component.getNotificationDisplayTimeMultiplier()
-                ? Visibility.SHOW : Visibility.HIDE;
     }
 
     private void renderBackgroundRow(GuiGraphics graphics, int width, int vOffset,
@@ -99,18 +108,18 @@ public class NotificationToast implements Toast {
         int uRemainder = Math.min(60, width - uWidth);
 
         // Left border
-        graphics.blitSprite(BACKGROUND_SPRITE, WIDTH, HEIGHT, 0, vOffset,
-                0, y, uWidth, vHeight);
+        graphics.blitSprite(RenderType::guiTextured, BACKGROUND_SPRITE, WIDTH, HEIGHT,
+                0, vOffset, 0, y, uWidth, vHeight);
 
         // Middle background
         int offset = 64;
         for (int x = uWidth; x < width - uRemainder; x += offset) {
-            graphics.blitSprite(BACKGROUND_SPRITE, WIDTH, HEIGHT, HEIGHT, vOffset,
-                    x, y, Math.min(offset, width - x - uRemainder), vHeight);
+            graphics.blitSprite(RenderType::guiTextured, BACKGROUND_SPRITE, WIDTH, HEIGHT,
+                    HEIGHT, vOffset, x, y, Math.min(offset, width - x - uRemainder), vHeight);
         }
 
         // Right border
-        graphics.blitSprite(BACKGROUND_SPRITE, WIDTH, HEIGHT, WIDTH - uRemainder, vOffset,
-                width - uRemainder, y, uRemainder, vHeight);
+        graphics.blitSprite(RenderType::guiTextured, BACKGROUND_SPRITE, WIDTH, HEIGHT,
+                WIDTH - uRemainder, vOffset, width - uRemainder, y, uRemainder, vHeight);
     }
 }
