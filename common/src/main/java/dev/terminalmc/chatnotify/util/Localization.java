f6b4df5af8bbd7712268b618a88c4dadb08052c1
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-07-03 13:07:56 +0800

    Add extra localization utils

--- common/src/main/java/dev/terminalmc/chatnotify/util/Localization.java
+++ common/src/main/java/dev/terminalmc/chatnotify/util/Localization.java
@@ -21,8 +21,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
 public final class Localization {
+
+    public static String translationKey(String path) {
+        return ChatNotify.MOD_ID + "." + path;
+    }
+
     public static String translationKey(String domain, String path) {
         return domain + "." + ChatNotify.MOD_ID + "." + path;
+    }
+
+    public static MutableComponent localized(String path, Object... args) {
+        return Component.translatable(translationKey(path), args);
     }
 
     public static MutableComponent localized(String domain, String path, Object... args) {
