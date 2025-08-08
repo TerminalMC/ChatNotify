2b1fc341df7e36fc29e6f14c6c21c16e780628b4
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-08-08 21:37:06 +0800

    Update to mc1.21.7

--- common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/root/DefaultList.java
+++ common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/root/DefaultList.java
@@ -155,9 +155,9 @@ public class DefaultList extends OptionList {
                                 hsv
                         );
                         if (hsv[2] < 0.1)
-                            colorField.setTextColor(0xFFFFFF);
+                            colorField.setTextColor(0xFFFFFFFF);
                         else
-                            colorField.setTextColor(color);
+                            colorField.setTextColor((0xFF << 24) | color);
                     }
                 });
                 colorField.setValue(((TextColorAccessor) (Object) TextColor.fromRgb(Config.get().defaultColor)).chatnotify$formatValue());
