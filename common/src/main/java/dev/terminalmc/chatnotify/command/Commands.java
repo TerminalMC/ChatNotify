9249927754424ba604c0a5852b981f98c480d2b8
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-10-16 10:20:57 +0800

    Update to mc1.21.3

--- common/src/main/java/dev/terminalmc/chatnotify/command/Commands.java
+++ common/src/main/java/dev/terminalmc/chatnotify/command/Commands.java
@@ -37,7 +37,7 @@ public class Commands {
         //noinspection unchecked
         dispatcher.register((LiteralArgumentBuilder<S>) literal(ChatNotify.MOD_ID)
                 .executes((ctx) -> {
-                    mc.tell(() -> mc.setScreen(new RootScreen(mc.screen)));
+                    mc.schedule(() -> mc.setScreen(new RootScreen(mc.screen)));
                     return Command.SINGLE_SUCCESS;
                 })
         );
