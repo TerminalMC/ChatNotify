d72de132e279e424d1b218fc88f95e757a7e71cd
NotRyken <127091011+NotRyken@users.noreply.github.com>
2026-01-14 18:57:33 +0800

    Update to mc1.21.8

--- common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/FilterList.java
+++ common/src/main/java/dev/terminalmc/chatnotify/gui/widget/list/FilterList.java
@@ -939,7 +939,7 @@ public class FilterList<E extends StringSupplier> extends DragReorderList {
                         Component.literal(Unicode.PAINT.str)
                                 .withColor(notif.textStyle.doColor
                                         ? notif.textStyle.color
-                                        : 0xffffff),
+                                        : 0xFFffffff),
                         (button) -> {
                             // Open color picker overlay widget
                             int cpHeight = HsvColorPicker.MIN_HEIGHT;
@@ -986,9 +986,9 @@ public class FilterList<E extends StringSupplier> extends DragReorderList {
                                     hsv
                             );
                             if (hsv[2] < 0.1)
-                                colorField.setTextColor(0xFFFFFF);
+                                colorField.setTextColor(0xFFFFFFFF);
                             else
-                                colorField.setTextColor(color);
+                                colorField.setTextColor((0xFF << 24) | color);
                             // Update status button color
                             colorEditButton.setMessage(colorEditButton.getMessage()
                                     .copy()
